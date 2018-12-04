5b21620473fa5b76079210fbaed66af174454601
FabianRei <f.abi.an@gmx.de>
2018-12-03 19:18:34 -0800

    update docker script to automatically correct wrong bvec. Requires that only one bvec, dwi etc is in the input.

--- docker_dti_init_neuro_detect/detectWithinDir.py
+++ docker_dti_init_neuro_detect/detectWithinDir.py
@@ -4,7 +4,13 @@ import torch
 from glob import glob
 import os
 import argparse
+import dipy.io as io
 
+
+def writeBvec(bvec, path):
+    writeString = "\n".join([" ".join([str(n) for n in vec]) for vec in bvec.transpose()])
+    with open(path, 'w') as f:
+        f.write(writeString)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', default='/flywheel/v0/output')
@@ -19,9 +25,21 @@ dimOut = 4
 net = SimpleNet(dimIn=dimIn, dimOut=dimOut).cpu()
 net.load_state_dict(torch.load(networkWeights, map_location='cpu'))
 archivPath = args.p
+flipDict = {1:'X', 2:'Y', 3:'Z'}
 for archiv in glob(os.path.join(archivPath, "dti*.zip")):
     flipPreds = predictFromArchive(archiv, os.path.join(archivPath, "neuro-detect_report.txt"), net)
-    for key, val in flipPreds.items():
-        parentDir = os.path.dirname(os.path.abspath(archivPath))
-        bvecDir = os.path.join(parentDir, "input/bvec")
-        print(os.listdir(bvecDir))
+    whereToFlip = list(flipPreds.values())[0]
+    parentDir = os.path.dirname(os.path.abspath(archivPath))
+    bvecDir = os.path.join(parentDir, "input/bvec")
+    bvecPath = glob(bvecDir + '/*.bvecs')[0]
+    originalName = os.path.basename(bvecPath)
+    originalName = os.path.splitext(originalName)[0]
+    if 0 < whereToFlip < 4:
+        bvec = io.read_bvals_bvecs(None, bvecPath)[1]
+        bvec[:,whereToFlip-1] = -bvec[:,whereToFlip-1]
+        outPath = os.path.join(archivPath, f"{originalName}_correctedBy{flipDict[whereToFlip]}.bvecs")
+        writeBvec(bvec, outPath)
+        print(f"flipped {originalName} by the {flipDict[whereToFlip]} axis.")
+        print(f"Wrote {outPath}")
+    else:
+        print(f"Didn't automatically correct {originalName}")
