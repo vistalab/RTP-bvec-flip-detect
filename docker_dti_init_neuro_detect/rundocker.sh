e2352e2d4d97ebf333467bee943c2341819982ea
garikoitz <garikoitz@gmail.com>
2019-03-01 09:08:29 -0800

    Commiting before building -

---
+++ docker_dti_init_neuro_detect/rundocker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash 
+
+docker run --rm -ti \
+    --entrypoint=/bin/bash \
+	-v /black/localhome/glerma/TESTDATA/bvecflipdetect/input:/flywheel/v0/input \
+	-v /black/localhome/glerma/TESTDATA/bvecflipdetect/output:/flywheel/v0/output \
+	-v /data/localhome/glerma/soft/neuro_detect/docker_dti_init_neuro_detect/example_config.json:/flywheel/v0/config.json vistalab/neuro-detect:0.4.1
