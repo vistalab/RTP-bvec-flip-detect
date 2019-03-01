d173a3cc8f4319f4567567ee0592f5e9fb034507
garikoitz <garikoitz@gmail.com>
2019-02-28 17:10:24 -0800

    Commiting before building -

--- docker_dti_init_neuro_detect/build.sh
+++ docker_dti_init_neuro_detect/build.sh
@@ -5,6 +5,6 @@ cd source/
 . ./compile.sh
 cd ../
 git add .
-git commit -m "Commiting before building $GEAR:$1"
+git commit -m "Commiting before building $GEAR-$1"
 GEAR=vistalab/neuro-detect
 docker build --tag $GEAR:$1 .
